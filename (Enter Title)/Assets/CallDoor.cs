2c006774176236bd11ca3540791dd97efc32e7e3
Brady <poolofclay33@gmail.com>
2019-03-06 11:42:04 -0600

    ...

---
+++ (Enter Title)/Assets/CallDoor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallDoor : MonoBehaviour
+{
+    public GameObject _door;
+
+    public void OpenDoor()
+    {
+        _door.GetComponent<Animator>().Play("OpenDoor(Tutorial)");
+    }
+}
--- (Enter Title)/Assets/Scripts/RotatePickup.cs
+++ (Enter Title)/Assets/Scripts/RotatePickup.cs
@@ -11,4 +11,18 @@ public class RotatePickup : MonoBehaviour
             GetComponent<MeshRenderer>().enabled = false;
         }
     }
+
+    float speed = 5f;
+    //adjust this to change how high it goes
+    float height = 0.5f;
+
+    void Update()
+    {
+        //get the objects current position and put it in a variable so we can access it later with less code
+        Vector3 pos = transform.position;
+        //calculate what the new Y position will be
+        float newY = Mathf.Sin(Time.time * speed);
+        //set the object's Y to the new calculated Y
+        transform.position = new Vector3(pos.x, newY, pos.z) * height;
+    }
 }
