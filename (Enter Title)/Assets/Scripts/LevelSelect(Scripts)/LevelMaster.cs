748b8ebe176147a4198b814bc12dac78feb05e5b
Brady Claypool <poolofclay33@gmail.com>
2019-03-07 16:21:27 -0600

    ...

--- (Enter Title)/Assets/Scripts/LevelSelect(Scripts)/LevelMaster.cs
+++ (Enter Title)/Assets/Scripts/LevelSelect(Scripts)/LevelMaster.cs
@@ -9,6 +9,7 @@ public class LevelMaster : MonoBehaviour
     public Material _blue;
 
     public GameObject _cube1;
+    public GameObject _playerCube;
 
     // Update is called once per frame
     void Update()
@@ -16,13 +17,28 @@ public class LevelMaster : MonoBehaviour
         
     }
 
+    private void Start()
+    {
+        _playerCube.GetComponent<LevelSelectCube>().enabled = false;
+        StartCoroutine("Wait");
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(2f);
+        GetComponent<Animator>().Play("BeginLevelSelect");
+        yield return new WaitForSeconds(4.5f);
+        _playerCube.GetComponent<LevelSelectCube>().enabled = true;
+    }
+
     public void Level1()
     {
+        GetComponent<Animator>().enabled = false;
+
         _cube1.GetComponent<Renderer>().material = _yellow;
         //_cube1.GetComponentInChildren<ParticleSystem>().Play();
         //SceneManager.LoadScene("Level01");
-
-        Debug.Log("MADEIt');");
+        //_cube1.GetComponent<MeshRenderer>().material = _yellow;
     }
 
     public void TestMat()
