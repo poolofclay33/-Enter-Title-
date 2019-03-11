6824a4986640625106b5c1a6550bc2f6569cc08e
Brady <poolofclay33@gmail.com>
2019-03-11 10:47:48 -0500

    Added Level02

---
+++ (Enter Title)/Assets/Obstacles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    public Material _red;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            transform.parent.GetComponent<Renderer>().material = _red;
+        }
+    }
+}
--- (Enter Title)/Assets/Scripts/Level01(Scripts)/GameManager.cs
+++ (Enter Title)/Assets/Scripts/Level01(Scripts)/GameManager.cs
@@ -11,12 +11,28 @@ public class GameManager : MonoBehaviour
 
     public GameObject[] _levelCubes;
 
+    public Canvas _pauseCanvas;
+    private bool _canOn = true;
+
     private void Update()
     {
         if(Finish._levelFinished[0] == true)
         {
             _levelManager.GetComponent<Animator>().Play("Level2Anim");
             _levelCubes[0].GetComponent<Renderer>().material = _levelComplete;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape) && _canOn == true)
+        {
+            Time.timeScale = 0;
+            _pauseCanvas.enabled = true;
+            _canOn = false;
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape) && _canOn == false)
+        {
+            Time.timeScale = 1;
+            _pauseCanvas.enabled = false;
+            _canOn = true;
         }
     }
 
